f28a681c0fe3f6437e6c22f9d4179e07a9a596f0
Roland Enola <40168844+mystiexx@users.noreply.github.com>
2019-08-16 13:49:52 +0100

    Create Content.js

---
+++ src/component/others/Content.js
@@ -0,0 +1,46 @@
+const Content = [
+    {
+        id: 1,
+        title: 'Freelancing As A Developer',
+        Description:'Things to know about being a freelancer',
+        Publisher: 'Traversy Media',
+
+    },
+    {
+        id: 2,
+        title: 'Adobe XD UI/UX',
+        Description:'Things to know about being a freelancer',
+        Publisher: 'Traversy Media',
+
+    },
+    {
+        id: 3,
+        title: 'Freelancing As A Developer',
+        Description:'Things to know about being a freelancer',
+        Publisher: 'Traversy Media',
+
+    },
+    {
+        id: 4,
+        title: 'Freelancing As A Developer',
+        Description:'Things to know about being a freelancer',
+        Publisher: 'Traversy Media',
+
+    },
+    {
+        id: 5,
+        title: 'Freelancing As A Developer',
+        Description:'Things to know about being a freelancer',
+        Publisher: 'Traversy Media',
+
+    },
+    {
+        id: 6,
+        title: 'Freelancing As A Developer',
+        Description:'Things to know about being a freelancer',
+        Publisher: 'Traversy Media',
+
+    },
+]
+
+export default Content;
