dacd2e7e2e6679354384398565ac4b3a2c5ef0f6
Roland Enola <40168844+mystiexx@users.noreply.github.com>
2019-08-16 13:49:47 +0100

    Create Header.js

---
+++ src/component/Nav/Header.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react'
+import logo from '../images/logo.png'
+import {Navbar, Nav, Button, Form} from 'react-bootstrap';
+
+
+
+class Header extends Component {
+    render() {
+        return (
+                <Navbar bg="dark" variant="dark" fixed="top" className="navbar justify-content-between" expand="lg">
+                    <Navbar.Brand href=""><img src={logo}/></Navbar.Brand>
+                    <Navbar.Toggle aria-controls="basic-navbar-nav"/>
+                    <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="ml-auto nav-link">
+                    <Navbar.Text className="link">DashBoard</Navbar.Text>
+                    </Nav>
+                    <Form inline>
+                    <Button variant="outline-primary">Log Out
+                    </Button>
+                    </Form>
+                    </Navbar.Collapse>
+                    </Navbar>
+                            )
+    }
+}
+export default Header;
